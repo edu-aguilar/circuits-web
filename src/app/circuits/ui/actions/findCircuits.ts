0180c44a3f5ee6c0c80db8d0ff9712e1be0770df
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2024-06-23 19:10:12 +0200

    feat: testing dynamic rendering

--- src/app/circuits/ui/actions/findCircuits.ts
+++ src/app/circuits/ui/actions/findCircuits.ts
@@ -1,6 +1,8 @@
 import { FindCircuitsUseCase } from "../../domain/usecases/FindCircuitsUseCase";
+import { unstable_noStore as noStore } from "next/cache";
 
 export const findCircuits = async () => {
+  noStore();
   const findCircuitsUseCase = new FindCircuitsUseCase();
 
   const circuits = await findCircuitsUseCase.execute();
