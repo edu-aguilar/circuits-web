d8bcdac63ad00ac45c862c33a2174e8860b4d511
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2024-06-28 08:51:19 +0200

    refactor: create component SearchInput

--- src/app/circuits/ui/components/CircuitFilters.tsx
+++ src/app/circuits/ui/components/CircuitFilters.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { SearchInput } from "@/app/common/ui/components/SearchInput";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 
@@ -23,38 +24,10 @@ export const CircuitFilters = () => {
 
   return (
     <div className="relative flex">
-      <div className="relative">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-          <svg
-            className="w-4 h-4 text-gray-500 dark:text-gray-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
-        </div>
-        <input
-          type="search"
-          id="search"
-          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="nombre circuito"
-          onChange={(event) => {
-            const value = event.target.value;
-            if (value.length > 2 || value.length === 0) {
-              handleSearch(value);
-            }
-          }}
-          defaultValue={circuitName}
-        />
-      </div>
+      <SearchInput
+        onChange={handleSearch}
+        defaultValue={circuitName}
+      ></SearchInput>
     </div>
   );
 };
