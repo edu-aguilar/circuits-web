2c064da565f6dd98b1110ff99491b5342b08dc15
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2024-09-23 17:53:38 +0200

    feat: update CircuitList to use EmptyState component

--- src/app/circuits/ui/components/CircuitList.tsx
+++ src/app/circuits/ui/components/CircuitList.tsx
@@ -1,3 +1,4 @@
+import { EmptyState } from "@/app/common/ui/components/EmptyState";
 import { CircuitFilterQuery } from "../../domain/types/CircuitFilterQuery";
 import { findCircuits } from "../actions/findCircuits";
 import { CircuitCard } from "./CircuitCard";
@@ -17,6 +18,9 @@ export const CircuitList = async ({ filters }: CircuitListProps) => {
       {circuits.map((circuit) => {
         return <CircuitCard key={circuit.id} circuit={circuit}></CircuitCard>;
       })}
+      {circuits.length === 0 && (
+        <EmptyState title="No se han encontrado circuitos" />
+      )}
     </>
   );
 };
