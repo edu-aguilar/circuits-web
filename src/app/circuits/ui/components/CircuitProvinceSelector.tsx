45f55f18a6f4f56b0e61593e8acf97e48c103ac6
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2024-09-30 16:24:57 +0200

    radix integration test

--- src/app/circuits/ui/components/CircuitProvinceSelector.tsx
+++ src/app/circuits/ui/components/CircuitProvinceSelector.tsx
@@ -3,6 +3,7 @@
 import { Province } from "@/app/common/domain/types/Province";
 import { Selector } from "@/app/common/ui/components/Selector";
 import { removeAccents } from "@/app/utils/removeAccents";
+import { Flex, Select } from "@radix-ui/themes";
 import { useSearchParams, usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -41,13 +42,34 @@ export const CircuitProvinceSelector = ({
   };
 
   return (
-    <Selector
-      items={provinces}
-      name="province"
-      id="provinceId"
-      onChange={handleProvinceChange}
-      defaultValue={selectedProvince ? selectedProvince.id : "0"}
-      anyValueLabel="Cualquier provincia"
-    />
+    <>
+      <Selector
+        items={provinces}
+        name="province"
+        id="provinceId"
+        onChange={handleProvinceChange}
+        defaultValue={selectedProvince ? selectedProvince.id : "0"}
+        anyValueLabel="Cualquier provincia"
+      />
+      <div className="flex flex-col w-32">
+        <Select.Root
+          size="3"
+          defaultValue={selectedProvince ? selectedProvince.id : "0"}
+          onValueChange={handleProvinceChange}
+        >
+          <Select.Trigger />
+          <Select.Content>
+            <Select.Group>
+              <Select.Item value="0">Cualquier Provincia</Select.Item>
+              {provinces.map((province) => (
+                <Select.Item key={province.id} value={province.id}>
+                  {province.name}
+                </Select.Item>
+              ))}
+            </Select.Group>
+          </Select.Content>
+        </Select.Root>
+      </div>
+    </>
   );
 };
