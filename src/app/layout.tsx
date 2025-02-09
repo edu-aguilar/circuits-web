7fe8c6abe9ee6fff8a838ab6cc06cc8d11bebaa1
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2025-02-09 17:57:30 +0100

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/favicon-180x180.png" />
+        <link rel="icon" href="/favicon-512x512.png" sizes="512x512" />
+        <link rel="manifest" href="/manifest.json" />
+
         <meta name="google-adsense-account" content="ca-pub-6477691629525796" />
         <AdSense pid="pub-6477691629525796" />
       </head>
